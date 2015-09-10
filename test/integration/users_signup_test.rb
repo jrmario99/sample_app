14df3a43eb0acd1df0b3009472e23c5a630514d2
mario <jrmario99@gmail.com>
2015-09-09 23:34:57 -0300

    finished exercise7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end 
+
+
+
    test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -19,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.FILL_IN
   end
 end
