e6a33ea887a2f869857a9d586651c40f13be106e
mario <jrmario99@gmail.com>
2015-09-14 19:56:11 -0300

    Reorganize

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module UsersHelper
 
   # Returns the Gravatar for the given user.
@@ -9,24 +8,5 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
-=======
-module SessionsHelper
-
-  # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
-  end
 
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-    # Returns true if the user is logged in, false otherwise.
-  def logged_in?
-    !current_user.nil?
-  end
-
-end
->>>>>>> log-in-log-out
